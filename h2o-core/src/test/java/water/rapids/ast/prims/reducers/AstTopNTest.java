4b852174fe8fb32ba8238e953b344e219f1fa3e2
wendycwong <wendycwong@yahoo.com>
2017-06-08 17:20:05 -0700

    PUBDEV-4567: top/bottomN speedup.  Change 0/1 to -1/1 to indicate grab top or bottom percentages.

--- h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstTopNTest.java
@@ -71,11 +71,11 @@ public class AstTopNTest extends TestUtil {
 								Log.info("Percentage is " + testPercent);
 								if (runTest[testIndex++]) {
 										Log.info("Testing top N long.");
-										testTopBottom(topLong, testPercent, 0, "0", _tolerance);
+										testTopBottom(topLong, testPercent, -1, "0", _tolerance);
 								}
 								if (runTest[testIndex++]) {
 										Log.info("Testing top N float.");
-										testTopBottom(topFloat, testPercent, 0, "1", _tolerance);  // test top % Float
+										testTopBottom(topFloat, testPercent, -1, "1", _tolerance);  // test top % Float
 								}
 								if (runTest[testIndex++]) {
 										Log.info("Testing bottom N long.");
